7c7c8ae62b1f133d32edf2f63e713d7ef2a71907
ukeyim <ukeyim@gmail.com>
2016-10-05 17:50:32 +0800

    ws2 demo

--- WS1/app/src/main/java/com/example/alex/ws1/MainActivity.java
+++ WS1/app/src/main/java/com/example/alex/ws1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.alex.ws1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     EditText txt_UserName, txt_UserPW;
@@ -16,11 +19,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         setContentView(R.layout.activity_main);
 
-        btn_Login = (Button)findViewById(R.id.btn_Login);
+        btn_Login = (Button) findViewById(R.id.btn_Login);
 
-        txt_UserName = (EditText)findViewById(R.id.txt_UserName);
+        txt_UserName = (EditText) findViewById(R.id.txt_UserName);
 
-        txt_UserPW = (EditText)findViewById(R.id.txt_UserPW);
+        txt_UserPW = (EditText) findViewById(R.id.txt_UserPW);
 
 // Register the Login button to click listener
 
@@ -42,14 +45,29 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             String upassword = txt_UserPW.getText().toString();
 
-            System.out.println( "@@@@@@@@@@@@@@@\n" +
+            System.out.println("@@@@@@@@@@@@@@@\n" +
 
                     "The Portal ID is: " + uname + "\n" +
 
                     "The Password is: " + upassword + "\n" +
 
-                    "@@@@@@@@@@@@@@@" );
-
+                    "@@@@@@@@@@@@@@@");
+            Intent intent = new Intent(getBaseContext(), CourseListActivity.class);
+            ArrayList<String> cname = new ArrayList<String>();
+            ArrayList<String> cteachers = new ArrayList < String > ();
+            ///**************Sample Datta**************//
+            cname.add("c11");
+            cteachers.add("t1");
+            cname.add("c22");
+            cteachers.add("t2");
+            cname.add("c33");
+            cteachers.add("t3");
+            cname.add("c44");
+            cteachers.add("t4");
+            ///**************Sample Datta**************//
+            intent.putStringArrayListExtra("CourseName", cname);
+            intent.putStringArrayListExtra("Teachers", cteachers);
+            startActivity(intent);
         }
 
     }
