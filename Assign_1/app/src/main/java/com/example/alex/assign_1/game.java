63a001b5e8c8857c42401408b4d246f82ef97598
ukeyim <ukeyim@gmail.com>
2016-10-22 18:50:29 +0800

    棋盘绘制

--- Assign_1/app/src/main/java/com/example/alex/assign_1/game.java
+++ Assign_1/app/src/main/java/com/example/alex/assign_1/game.java
@@ -1,18 +1,52 @@
 package com.example.alex.assign_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class game extends AppCompatActivity {
+    private String stdid, stdpw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index);
+        setContentView(R.layout.activity_game);
+//        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.gameRelativeLayout);//获取root RL
+//        LinearLayout linearLayout1 = new LinearLayout(this);
+//        Display display = getWindowManager().getDefaultDisplay(); 之后全部用代码画
 
-        TextView tv = new TextView(this);
-        tv.setText("niceboat");
-        setContentView(tv);
+
+//        Intent intent = getIntent();
+//        stdid = intent.getStringExtra("ID");
+//        stdpw = intent.getStringExtra("PW");
+//        TextView tv = new TextView(this);
+//        tv.setText(stdid + '\n' + stdpw);
+//        setContentView(tv);
+
+        LinearLayout linearLayout[]= new LinearLayout[7];
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout[0] = (LinearLayout)findViewById(R.id.ll1);
+        linearLayout[1] = (LinearLayout)findViewById(R.id.ll2);
+        linearLayout[2] = (LinearLayout)findViewById(R.id.ll3);
+        linearLayout[3] = (LinearLayout)findViewById(R.id.ll4);
+        linearLayout[4] = (LinearLayout)findViewById(R.id.ll5);
+        linearLayout[5] = (LinearLayout)findViewById(R.id.ll6);
+        linearLayout[6] = (LinearLayout)findViewById(R.id.ll7);
+
+        ImageView imageButton;
+        for (int i = 0; i < 7; i++) {
+//            linearLayout[i]= (LinearLayout)findViewById(R.id.ll);
+            for (int j = 0; j < 6; j++) {
+                imageButton = new ImageView(this);
+                imageButton.setLayoutParams(new ViewGroup.LayoutParams(150, 150));
+                imageButton.setImageResource(R.drawable.empty_t);
+                linearLayout[i].addView(imageButton);
+//        TextView tv = new TextView(this);
+            }
+        }
     }
+//        setContentView(R.layout.activity_game);
 }
--- Assign_1/app/src/main/java/com/example/alex/assign_1/index.java
+++ Assign_1/app/src/main/java/com/example/alex/assign_1/index.java
@@ -1,17 +1,31 @@
 package com.example.alex.assign_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class index extends AppCompatActivity {
-
+    TextView stdid,stdpw;
+    Button btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-
-//        TextView tv = new TextView(this);
-//        tv.setText("niceboat");
-//        setContentView(tv);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        stdid = (TextView) findViewById(R.id.t_stdid);
+        stdpw = (TextView) findViewById(R.id.t_stdpw);
+        btn_start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){ //wtf is arg0, Y view here
+                Intent myIntent = new Intent(view.getContext(),game.class);
+                myIntent.putExtra("ID",stdid.getText().toString());
+                myIntent.putExtra("PW",stdpw.getText().toString());
+                startActivityForResult(myIntent,0);
+            }
+        });
     }
 }
