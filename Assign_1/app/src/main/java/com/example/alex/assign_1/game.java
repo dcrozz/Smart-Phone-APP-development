cd5c829a836a359c81ad0a39113797115cf0d8cf
ukeyim <ukeyim@gmail.com>
2016-11-02 22:47:43 +0800

    上下左右判断逻辑

--- Assign_1/app/src/main/java/com/example/alex/assign_1/game.java
+++ Assign_1/app/src/main/java/com/example/alex/assign_1/game.java
@@ -9,7 +9,9 @@ import android.widget.LinearLayout;
 
 public class game extends AppCompatActivity {
     private String stdid, stdpw;
-    private boolean isWhiteTurn;
+    private boolean isRedTurn;
+    private int[][] chess = new int[7][6];
+    //none = 0, red = 1, green =2，fin = 3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +19,13 @@ public class game extends AppCompatActivity {
 //        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.gameRelativeLayout);//获取root RL
 //        LinearLayout linearLayout1 = new LinearLayout(this);
 //        Display display = getWindowManager().getDefaultDisplay(); 之后全部用代码画
-
-
 //        Intent intent = getIntent();
 //        stdid = intent.getStringExtra("ID");
 //        stdpw = intent.getStringExtra("PW");
 //        TextView tv = new TextView(this);
 //        tv.setText(stdid + '\n' + stdpw);
 //        setContentView(tv);
-
         LinearLayout linearLayout[]= new LinearLayout[7];
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-//                LinearLayout.LayoutParams.WRAP_CONTENT);
         linearLayout[0] = (LinearLayout)findViewById(R.id.ll1);
         linearLayout[1] = (LinearLayout)findViewById(R.id.ll2);
         linearLayout[2] = (LinearLayout)findViewById(R.id.ll3);
@@ -36,17 +33,14 @@ public class game extends AppCompatActivity {
         linearLayout[4] = (LinearLayout)findViewById(R.id.ll5);
         linearLayout[5] = (LinearLayout)findViewById(R.id.ll6);
         linearLayout[6] = (LinearLayout)findViewById(R.id.ll7);
-
         ImageView imageButton[][] = new ImageView[7][6];
-        final boolean[][] chess = new boolean[7][6];
-        for (int i = 0; i < 7; i++){
-            for(int j=0;j<6; j++){
-                chess[i][j] = false;
+        for(int i = 0; i < 7; i++){
+            for(int j=0; j < 6; j++){
+                chess[i][j] = 0;
             }
         }
-//        chess[0][0] = false;
-        for (int i = 6; i >=0 ; i--) {
-            for (int j = 5; j >= 0; j--) {
+        for (int i = 0; i <7 ; i++) {
+            for (int j = 0; j <6; j++) {
                 final int locali = i;
                 final int localj = j;
                 final ImageView localimageButton[][] = imageButton;
@@ -56,18 +50,20 @@ public class game extends AppCompatActivity {
                 imageButton[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (!chess[locali][localj]) {
-                            for (int count = 0; count < 6 ; count++) {
-                                if (!chess[locali][count]) {
-                                    if (isWhiteTurn) {
+                        if (chess[locali][localj] == 0) {
+                            for (int count = 5; count >= 0 ; count--) {
+                                if (chess[locali][count] == 0) {
+                                    if (isRedTurn) {
                                         localimageButton[locali][count].setImageResource(R.drawable.red_t);
-                                        isWhiteTurn = false;
-                                        chess[locali][count] = true;
+                                        isRedTurn = false;
+                                        chess[locali][count] = 1;
+                                        ifWin(locali,count,1);
                                         break;
                                     } else {
                                         localimageButton[locali][count].setImageResource(R.drawable.green_t);
-                                        isWhiteTurn = true;
-                                        chess[locali][count] = true;
+                                        isRedTurn = true;
+                                        chess[locali][count] = 2;
+                                        ifWin(locali,count,2);
                                         break;
                                     }
                                 }
@@ -76,10 +72,91 @@ public class game extends AppCompatActivity {
                     }
                 });
                 linearLayout[i].addView(imageButton[i][j]);
-//        TextView tv = new TextView(this);
             }
         }
-
     }
-//        setContentView(R.layout.activity_game);
+    protected void ifWin(int i, int j, int color){
+//        int lcount = 0;
+//        int rcount = 0;
+//        int dcount = 0;
+        int ulcount = 0;
+//        int urcount = 0;
+//        int dlcount = 0;
+        int drcount = 0;
+//            for(int x=i;x>=0;x--){
+//                //判左边
+//                if(chess[x][j]==color){
+//                    lcount++;
+//                    System.out.println(Integer.toString(color) + "lcount:" + Integer.toString(lcount));
+//                }else{
+//                    continue;
+//                }
+//            }
+//            for(int x=i;x<7;x++){
+//                //判右边
+//                if(chess[x][j]==color){
+//                    rcount++;
+//                    System.out.println(Integer.toString(color) + "rcount:" + Integer.toString(rcount));
+//                }else{
+//                    continue;
+//                }
+//            }
+//            if(lcount+rcount==5){
+//                if(color==1){
+//                    System.out.print("Red WIN");
+//                }else{
+//                    System.out.print("Green WIN");
+//                }
+//            }
+
+//        for(int x=j;x<6;x++){
+//            if(chess[i][x]==color){
+//                dcount++;
+//                System.out.println(Integer.toString(color) + "dcount:" + Integer.toString(dcount));
+//                if(dcount==4){
+////                    for(int a = 0; a < 7; i++){
+////                        for(int b=0; b < 6; j++){
+////                            chess[a][b] = 3;
+////                        }
+////                    }
+//                    if(color==1){
+//                        System.out.print("Red WIN");
+//                    }else{
+//                        System.out.print("Green WIN");
+//                    }
+//                }
+//            }else{
+//                continue;
+//            }
+//        }
+        int tmpj = j;
+        for(int x=i;x>=0&&j>0;x--,tmpj--){
+            //判左上
+            if(x==0 || tmpj== 0){ulcount = 0;break;}
+            if(chess[x][tmpj]==color){
+                ulcount++;
+                System.out.println("------->"+x+j+"----->"+ulcount);
+            }else{
+                break;
+            }
+        }
+        tmpj = j;
+        for(int x=i;x<6&&j<5;x++,tmpj++){
+            //判右下
+            if(x==6 || tmpj== 5){drcount = 0; break;}
+            if(chess[x+1][tmpj+1]==color){
+                drcount++;
+                System.out.println("------->"+x+j+"----->"+drcount);
+            }else{
+                break;
+            }
+        }
+        if(ulcount+drcount>=4){
+            if(color==1){
+                System.out.print("Red WIN");
+            }else{
+                System.out.print("Green WIN");
+            }
+        }
+    }
 }
